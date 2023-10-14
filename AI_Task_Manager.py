aadc0e0bcb9ac8eb448472eedcf4c0be143cb4e1
arora-kunal <kunal.22SCSE1180152@galgotiasuniversity.edu.in>
2023-10-14 21:28:44 +0530

    Voice Support Added

--- AI_Task_Manager.py
+++ AI_Task_Manager.py
@@ -1,228 +1,126 @@
-import datetime
-import time
-import requests
-from telegram import Bot, Update
-from telegram.ext import CommandHandler, CallbackContext, Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+import telegram
 import openai
-import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
-import re
+from moviepy.editor import AudioFileClip
+import concurrent.futures
 
-openai.api_key = "Your OpenAI API Key"
+# Initialize OpenAI and Telegram API tokens
+openai.api_key = "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"
+TELEGRAM_API_TOKEN = "6432141709:AAEnyt3zns3yL8F2fsnKh85C4RZmoOiqKV4"
 
-class TaskManager:
-    def __init__(self):
-        self.tasks = []
+# Create a list to store the conversation history
+conversation = []
 
-    def add_task(self, task):
-        self.tasks.append(task)
+# Define the system message
+system_message = {
+    "role": "system",
+    "content": "You are AISmartTask: An innovative AI-driven task management solution tailored for coders or anyone who likes to manage daily routines. You offer an intuitive user experience. Key features include dynamic task management that allows for task addition, prioritization, and AI-powered task suggestions. You can accept documents in the form of text, and your document parser extracts essential summaries and highlights critical keywords from that document. Additionally, AISmartTask visualizes task priorities, enabling users to get a clearer view of their tasks at hand. Your one standout quality is your learning modules, which web scrape for knowledge, encouraging continuous learning. Users can interact with you and provide feedback for continuous system improvement. AISmartTask is not just a task manager; it's a revolutionary tool aiming to optimize and enhance the daily routines of coders. It is scalable to any user and other applications such as enterprise project management, academic tasks management (students and educators), task management in healthcare, and in the freelance and gig economy."
+}
 
-    def prioritize_tasks(self):
-        # Implement prioritization logic (e.g., based on priority attribute and deadline)
-        self.tasks = sorted(self.tasks, key=lambda task: (task.priority, task.deadline))
+# Initialize the conversation with the system message
+conversation.append(system_message)
 
-    def organize_tasks(self):
-        # Implement task organization logic (e.g., categorizing tasks)
-        pass
+def text_message(update, context):
+    user_message = update.message.text
+    # Append user message to the conversation
+    conversation.append({"role": "user", "content": user_message})
 
-    def generate_recurring_tasks(self, user_input, feedback):
-        # Implement recurring task generation logic based on user input and feedback
-        # For example, based on user input like "I want to exercise daily" and feedback like "Make exercising a recurring task."
-        pass
-
-    def get_upcoming_tasks(self, days=1):
-        current_time = datetime.datetime.now()
-        end_time = current_time + datetime.timedelta(days=days)
-        upcoming_tasks = [task for task in self.tasks if current_time <= task.deadline <= end_time]
-        return upcoming_tasks
-
-class Task:
-    def __init__(self, name, deadline, priority):
-        self.name = name
-        self.deadline = deadline
-        self.priority = priority
-
-def call_notification(update: Update, context: CallbackContext, event):
-    user = update.message.from_user.username
-    event_time = datetime.datetime.fromisoformat(event["start"]["dateTime"])
-    call_time = event_time - datetime.timedelta(minutes=15)
-    current_time = datetime.datetime.now()
-
-    if current_time < call_time:
-        time_difference = call_time - current_time
-        time.sleep(time_difference.total_seconds())
-        text = "This is a robot calling you to remind you about your meeting."
-        lang = "en-GB-Standard-B"
-        url = f"http://api.callmebot.com/start.php?user=@{user}&text={text}&lang={lang}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            update.message.reply_text("Call initiated!")
-        else:
-            update.message.reply_text("Error initiating call!")
-
-def generate_task_suggestions(task_description):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Given the task description: '{task_description}', suggest ways to complete it.",
-        max_tokens=50,
-    )
-    return response.choices[0].text
-
-def document_parser(document_text, keywords):
-    # Implement document parsing, interpretation, and summarization
-    # Highlight keywords within the document
-    # This is a placeholder; you can use NLP libraries like spaCy or NLTK
-    summary = "Document summary goes here."
-    highlighted_document = document_text  # Placeholder for highlighting keywords
-    return summary, highlighted_document
-
-def create_task_time_allocation_visualization(tasks):
-    # Implement a function to generate visualizations based on task and time allocation data
-    task_names = [task.name for task in tasks]
-    task_priorities = [task.priority for task in tasks]
-
-    plt.figure(figsize=(10, 6))
-    plt.barh(task_names, task_priorities, color='skyblue')
-    plt.xlabel('Priority')
-    plt.title('Task Prioritization')
-    plt.tight_layout()
-
-    # Save or display the visualization
-    plt.savefig('task_prioritization.png')
-    # Alternatively, you can display it using plt.show()
-
-def send_upcoming_task_notifications(context):
-    upcoming_tasks = task_manager.get_upcoming_tasks()
-    if upcoming_tasks:
-        task_list = "\n".join([f"Name: {task.name}, Priority: {task.priority}, Deadline: {task.deadline.strftime('%Y-%m-%d')}" for task in upcoming_tasks])
-        context.bot.send_message(chat_id=context.job.context.effective_chat.id, text="Upcoming tasks:\n" + task_list)
-
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Hello! I am your AI Task Manager bot. Send /help for available commands.')
-
-def help_command(update: Update, context: CallbackContext):
-    help_message = """
-Available commands:
-- /start: Start the AI Task Manager.
-- /help: Show available commands.
-- /add_task: Add a new task (e.g., /add_task Buy groceries, 2023-12-31, 2)
-- /list_tasks: List tasks.
-- /delete_task: Delete a task by ID (e.g., /delete_task 1)
-- /update_task: Update a task by ID (e.g., /update_task 1, New Task, 2023-12-31, 2)
-- /document: Analyze a document with keywords (e.g., /document Sample document text. Keyword1; Keyword2; Keyword3)
-- /feedback: Provide feedback on AI decisions.
-- /upcoming_tasks: List upcoming tasks.
-"""
-    update.message.reply_text(help_message)
-
-def add_task(update: Update, context: CallbackContext):
-    update.message.reply_text("Please provide the task details in the format: Name, Deadline (YYYY-MM-DD), Priority (e.g., Buy groceries, 2023-12-31, 2)")
-
-def handle_add_task(update: Update, context: CallbackContext):
-    task_info = update.message.text.split(', ')
-    if len(task_info) == 3:
-        task_name, deadline_str, priority_str = task_info
-        try:
-            deadline = datetime.datetime.fromisoformat(deadline_str)
-            priority = int(priority_str)
-            new_task = Task(task_name, deadline, priority)
-            task_manager.add_task(new_task)
-            task_manager.prioritize_tasks()
-            update.message.reply_text("Task added successfully.")
-        except ValueError:
-            update.message.reply_text("Invalid date or priority format. Please use the format: Name, Deadline (YYYY-MM-DD), Priority")
-    else:
-        update.message.reply_text("Invalid task format. Please use the format: Name, Deadline (YYYY-MM-DD), Priority")
-
-def list_tasks(update: Update, context: CallbackContext):
-    task_list = "\n".join([f"ID: {i+1}, Name: {task.name}, Priority: {task.priority}, Deadline: {task.deadline.strftime('%Y-%m-%d')}" for i, task in enumerate(task_manager.tasks)])
-    update.message.reply_text("Your tasks:\n" + task_list)
-
-def delete_task(update: Update, context: CallbackContext):
-    task_id = int(context.args[0]) - 1
-    if 0 <= task_id < len(task_manager.tasks):
-        deleted_task = task_manager.tasks.pop(task_id)
-        update.message.reply_text(f"Task '{deleted_task.name}' deleted successfully.")
-    else:
-        update.message.reply_text("Invalid task ID. Please use /list_tasks to see the task list and provide a valid ID.")
-
-def update_task(update: Update, context: CallbackContext):
-    args = context.args
-    if len(args) != 4:
-        update.message.reply_text("Invalid format. Please use the format: ID, New Name, New Deadline (YYYY-MM-DD), New Priority")
-        return
-
-    task_id = int(args[0]) - 1
-    if 0 <= task_id < len(task_manager.tasks):
-        try:
-            new_name = args[1]
-            new_deadline = datetime.datetime.fromisoformat(args[2])
-            new_priority = int(args[3])
-            task_manager.tasks[task_id].name = new_name
-            task_manager.tasks[task_id].deadline = new_deadline
-            task_manager.tasks[task_id].priority = new_priority
-            task_manager.prioritize_tasks()
-            update.message.reply_text("Task updated successfully.")
-        except (ValueError, IndexError):
-            update.message.reply_text("Invalid date or priority format or task ID. Please use the format: ID, New Name, New Deadline (YYYY-MM-DD), New Priority")
-    else:
-        update.message.reply_text("Invalid task ID. Please use /list_tasks to see the task list and provide a valid ID.")
-
-def document(update: Update, context: CallbackContext):
-    update.message.reply_text("Please provide the document text followed by keywords separated by semicolons (e.g., /document Sample document text. Keyword1; Keyword2; Keyword3)")
-
-def handle_document(update: Update, context: CallbackContext):
-    text_and_keywords = update.message.text.split(' ')
-    if len(text_and_keywords) < 3:
-        update.message.reply_text("Invalid format. Please provide the document text followed by keywords separated by semicolons.")
-        return
-
-    document_text = text_and_keywords[1]
-    keywords = text_and_keywords[2].split(';')
-    document_summary, highlighted_document = document_parser(document_text, keywords)
-    update.message.reply_text("Document Analysis:\n" + document_summary)
-    update.message.reply_text("Highlighted Document:\n" + highlighted_document)
-
-def upcoming_tasks(update: Update, context: CallbackContext):
-    upcoming_tasks = task_manager.get_upcoming_tasks()
-    if upcoming_tasks:
-        task_list = "\n".join([f"Name: {task.name}, Priority: {task.priority}, Deadline: {task.deadline.strftime('%Y-%m-%d')}" for task in upcoming_tasks])
-        update.message.reply_text("Upcoming tasks:\n" + task_list)
-    else:
-        update.message.reply_text("No upcoming tasks.")
-
-def feedback_system(update: Update, context: CallbackContext):
-    update.message.reply_text("Please provide feedback on AI decisions.")
-    context.user_data['feedback'] = True
-
-def handle_feedback(update: Update, context: CallbackContext):
-    feedback = update.message.text
-    # Implement feedback processing and refinement of AI decisions
-    context.user_data['feedback'] = False
-    update.message.reply_text("Thank you for your feedback!")
-
-def main():
-    global task_manager
-    task_manager = TaskManager()
-
-    global updater
-    updater = Updater("Your Telegram Bot Token")
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help_command))
-    dp.add_handler(CommandHandler("add_task", add_task))
-    dp.add_handler(CommandHandler("list_tasks", list_tasks))
-    dp.add_handler(CommandHandler("delete_task", delete_task, pass_args=True))
-    dp.add_handler(CommandHandler("update_task", update_task, pass_args=True))
-    dp.add_handler(CommandHandler("document", document))
-    dp.add_handler(MessageHandler(Filters.text & Filters.command, handle_feedback))
-    dp.add_handler(CommandHandler("upcoming_tasks", upcoming_tasks))
+    # Use a maximum of the last 10 messages for context
+    context_messages = conversation[-20:]
     
-    job_queue = updater.job_queue
-    job_queue.run_repeating(send_upcoming_task_notifications, interval=3600, context=updater)
+    # Send user message to OpenAI for a response
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=context_messages
+    )
+    assistant_reply = response["choices"][0]["message"]["content"]
+    
+    # Send the assistant's reply back to the user
+    update.message.reply_text(text=f"**{assistant_reply}", parse_mode=telegram.ParseMode.MARKDOWN)
+    
+    # Append assistant's reply to the conversation
+    conversation.append({"role": "assistant", "content": assistant_reply})
+
+def voice_message(update, context):
+    update.message.reply_text("I've received your voice message! Please give me a second to respond :)")
+    
+    # Download the voice message and transcribe it
+    voice_file = context.bot.getFile(update.message.voice.file_id)
+    voice_file.download("voice_message.ogg")
+    audio_clip = AudioFileClip("voice_message.ogg")
+    audio_clip.write_audiofile("voice_message.mp3")
+    audio_file = open("voice_message.mp3", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file).text
+    
+    # Append the transcribed message to the conversation
+    conversation.append({"role": "user", "content": transcript})
+    
+    # Use a maximum of the last 10 messages for context
+    context_messages = conversation[-10:]
+    
+    # Send the transcribed message to OpenAI for a response
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=context_messages
+    )
+    assistant_reply = response["choices"][0]["message"]["content"]
+    
+    # Send the assistant's reply back to the user
+    update.message.reply_text(text=f"**{assistant_reply}", parse_mode=telegram.ParseMode.MARKDOWN)
+    
+    # Append assistant's reply to the conversation
+    conversation.append({"role": "assistant", "content": assistant_reply})
+
+def show_task_list(update, context):
+    # Extract and show the tasks from the conversation history
+    tasks = extract_tasks(conversation)
+    if tasks:
+        task_list = "\n".join(tasks)
+        update.message.reply_text(f"Your task list:\n{task_list}")
+    else:
+        update.message.reply_text("You don't have any tasks in your list yet.")
+
+def extract_tasks(conversation):
+    # Extract tasks from the conversation history
+    tasks = []
+    for message in conversation:
+        if message['role'] == 'user' and "add task" in message['content'].lower():
+            task = message['content'].replace("add task", "").strip()
+            tasks.append(task)
+    return tasks
+
+def start(update, context):
+    user_message = update.message.text
+    conversation.append({"role": "user", "content": user_message})
+    context_messages = conversation[-10:]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=context_messages
+    )
+    assistant_reply = response["choices"][0]["message"]["content"]
+
+    update.message.reply_text(text=f"**{assistant_reply}", parse_mode=telegram.ParseMode.MARKDOWN)
+    conversation.append({"role": "assistant", "content": assistant_reply})
+
+def prioritize_tasks(tasks):
+    # Placeholder function for task prioritization
+    # You can implement your task prioritization logic here
+    # For example, you can use NLP techniques to analyze tasks and prioritize based on deadlines, importance, etc.
+    return tasks
+
+def start_bot():
+    updater = Updater(TELEGRAM_API_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Add message handlers for text, voice, and showing task list
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), text_message))
+    dispatcher.add_handler(MessageHandler(Filters.voice, voice_message))
+    dispatcher.add_handler(CommandHandler("show_tasks", show_task_list))
 
     updater.start_polling()
     updater.idle()
 
 if __name__ == "__main__":
-    main()
+    start_bot()
